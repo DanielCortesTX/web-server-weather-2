3cf49ee013d33cfd14cf824bef8122f115a42894
Daniel Cortes <d5t5c5@yahoo.com>
2019-08-28 15:38:20 -0500

    feat: basic assets and handlebars

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Andrew Mead'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Andrew Mead'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'You are being helped'
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello Express!</h1>')
