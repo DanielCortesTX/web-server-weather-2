cc9482eccdc33465cf0573215516bd9ea632e471
Daniel Cortes <d5t5c5@yahoo.com>
2019-08-28 16:01:43 -0500

    feat: Added partials for header and footer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+
+// Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -24,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    message: 'You are being helped'
+    message: 'You are being helped',
+    title: 'Help',
+    name: 'Andrew Mead'
   })
 })
 
