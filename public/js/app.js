4a83d3a42b296bf5c5999f0cc32dedde572ff5c2
Daniel Cortes <d5t5c5@yahoo.com>
2019-09-08 17:41:23 -0500

    Setup App for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,32 @@
 console.log('Client-side js file is loaded')
 
-// fetch('http://localhost:3000/weather?address=philadelphia').then((response) => {
-//   response.json().then((data) => {
-//     if(data.error){
-//       console.log(data.error)
-//     } else {
-//       console.log(data.location)
-//       console.log(data.forecastData)
-//     }
-//   })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = 'From Javascript'
+messageTwo.textContent = 'also from javascript'
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = search.value
+
+  messageOne.textContent = 'Loading'
+  messageTwo.textContent = ''
   
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
   response.json().then((data) => {
-    if(data.error){
-      console.log(data.error)
+    const { location, forecastData, error } = data
+    if(error){
+      console.log(error)
+      messageOne.textContent = error
+      messageTwo.textContent = error
     } else {
-      console.log(data.location)
-      console.log(data.forecastData)
+      console.log(location)
+      messageOne.textContent = location
+      console.log(forecastData)
+      messageTwo.textContent = forecastData
     }
   })
 })
