3ab93ec82f924aae0b2916bed00d6297ff0c1983
Daniel Cortes <d5t5c5@yahoo.com>
2019-09-08 17:11:02 -0500

    feat: Made form for address input and submission handling

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,24 @@
 console.log('Client-side js file is loaded')
 
-fetch('http://localhost:3000/weather?address=philadelphia').then((response) => {
+// fetch('http://localhost:3000/weather?address=philadelphia').then((response) => {
+//   response.json().then((data) => {
+//     if(data.error){
+//       console.log(data.error)
+//     } else {
+//       console.log(data.location)
+//       console.log(data.forecastData)
+//     }
+//   })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+  
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
   response.json().then((data) => {
     if(data.error){
       console.log(data.error)
@@ -9,5 +27,6 @@ fetch('http://localhost:3000/weather?address=philadelphia').then((response) => {
       console.log(data.forecastData)
     }
   })
+})
 })
 // fetch is not a part of js. Not accessible in node.js. CLient side
