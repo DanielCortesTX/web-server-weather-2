2fec72bfc6d20754eda40cc36990bfba81ca376d
Daniel Cortes <d5t5c5@yahoo.com>
2019-09-08 09:38:40 -0500

    feat: Added fetch on index.js page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client-side js file is loaded')
+
+fetch('http://localhost:3000/weather?address=philadelphia').then((response) => {
+  response.json().then((data) => {
+    if(data.error){
+      console.log(data.error)
+    } else {
+      console.log(data.location)
+      console.log(data.forecastData)
+    }
+  })
+})
+// fetch is not a part of js. Not accessible in node.js. CLient side
